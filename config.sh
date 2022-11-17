8a2f78dcd86e087192a4bf34faf1f9f76889e706
amaapp333 <118077998+amaapp333@users.noreply.github.com>
2022-11-17 17:52:58 +0200

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,3 @@
+# Create the runner and start the configuration experience
+$ ./config.sh --url https://github.com/amaapp333/hennessey --token A4E3ULTRGIA7KGYOICNURTDDOZTAQ# Last step, run it!
+$ ./run.sh
